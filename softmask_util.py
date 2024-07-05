0171b5131ba136a16e65c965af43d9b4f98c6201
Bùi Huỳnh Quốc AnH <117139949+bhqanhuit@users.noreply.github.com>
2024-07-05 13:20:24 +0700

    update soft mask generation
    update soft mask generation

--- softmask_util.py
+++ softmask_util.py
@@ -4,6 +4,7 @@ import numpy as np
 from torchvision import transforms
 from models.face_parsing.model import BiSeNet
 from torchvision.utils import save_image
+from PIL import Image
 
 transform_ = transforms.Compose([
         transforms.ToTensor(),
@@ -40,13 +41,14 @@ def extract_hair_region(mask):
     hair_mask = (mask == hair_class_index).astype(np.uint8)
     return hair_mask
 
-def soft_boundary(mask, boundary_width=3):
+def soft_boundary(mask, boundary_width=13):
     kernel = np.ones((boundary_width, boundary_width), np.uint8)
     dilated = cv2.dilate(mask, kernel, iterations=1)
     eroded = cv2.erode(mask, kernel, iterations=1)
     boundary = dilated - eroded
     soft_mask = cv2.GaussianBlur(boundary.astype(np.float32), (boundary_width, boundary_width), 0)
-    return soft_mask + mask
+
+    return soft_mask
 
 def main(image_path, model_path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -56,16 +58,22 @@ def main(image_path, model_path):
     hair_mask = extract_hair_region(mask)
     soft_mask = soft_boundary(hair_mask)
 
-
-
-    cv2.imwrite('hard_mask.png', mask)
+    hair_mask = transform_(1 - hair_mask).float()
+    hair_mask = torch.where(hair_mask > 0, 1.0, 0.0)
     
-    save_image(transform_(soft_mask), 'soft_mask.png', normalize=True)
+    soft_mask = transform_(1 - soft_mask)
+    # for i in soft_mask:
+    #     for j in i:
+    #         for k in j:
+    #             print(k)
     
-
+    save_image(soft_mask, 'soft_mask.png', normalize=True)
+    save_image(hair_mask, 'hard_mask.png')
+    
     print("Soft mask saved as 'soft_mask.png'")
 
 if __name__ == "__main__":
-    image_path = "input/6.png"
+    image_path = "real_input.jpg"
     model_path = "pretrained_models/BiSeNet/face_parsing_79999_iter.pth"
+
     main(image_path, model_path)
